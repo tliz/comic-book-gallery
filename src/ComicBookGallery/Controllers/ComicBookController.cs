47cdbfecc6b316b272987f231f8862dbe4ac8aef
Tliz <tlizzen@gmail.com>
2016-11-28 18:13:17 +0300

    Updated Detail action method to use action result types

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -9,9 +9,34 @@ namespace ComicBookGallery.Controllers
     public class ComicBookController : Controller
         // : Controller - > inherit from base class
     {
-        public string Detail()
+        // public string Detail()
+        //{
+             //return "Hello from comic books controller!";
+        //}
+        //public ContentResult Detail()
+        public ActionResult Detail()
         {
-            return "Hello from comic books controller!";
+            if ( DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                // casting action result type to contentResult
+                //return (ContentResult) new RedirectResult("/");
+                // redirectResult object
+                // return new RedirectResult("/");
+
+                // call to redirect method
+                return Redirect("/");
+            }
+
+            // content result method
+            // instance of content result class
+            //return new ContentResult()
+            //{
+            // use content result content property to set content
+            //  Content = "Hello from comic books controller!"
+            // };  
+
+            // replace contentResult method with call to content object 
+            return Content("Hello from comic books controller!");
         }
     }
 }
