0db5d617851c8947fc70e2a4dc500b27980e25cb
Tliz <tlizzen@gmail.com>
2016-11-29 08:35:31 +0300

    Added Comic Book Detail View

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -11,32 +11,37 @@ namespace ComicBookGallery.Controllers
     {
         // public string Detail()
         //{
-             //return "Hello from comic books controller!";
+        //return "Hello from comic books controller!";
         //}
         //public ContentResult Detail()
+        // public ActionResult Detail()
+        //{
+        // if ( DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+        //   {
+        // casting action result type to contentResult
+        //return (ContentResult) new RedirectResult("/");
+        // redirectResult object
+        // return new RedirectResult("/");
+
+        // call to redirect method
+        //     return Redirect("/");
+        //  }
+
+        // content result method
+        // instance of content result class
+        //return new ContentResult()
+        //{
+        // use content result content property to set content
+        //  Content = "Hello from comic books controller!"
+        // };  
+
+        // replace contentResult method with call to content object 
+        //return Content("Hello from comic books controller!");
+        //}
+
         public ActionResult Detail()
         {
-            if ( DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                // casting action result type to contentResult
-                //return (ContentResult) new RedirectResult("/");
-                // redirectResult object
-                // return new RedirectResult("/");
-
-                // call to redirect method
-                return Redirect("/");
-            }
-
-            // content result method
-            // instance of content result class
-            //return new ContentResult()
-            //{
-            // use content result content property to set content
-            //  Content = "Hello from comic books controller!"
-            // };  
-
-            // replace contentResult method with call to content object 
-            return Content("Hello from comic books controller!");
+            return View();
         }
     }
 }
