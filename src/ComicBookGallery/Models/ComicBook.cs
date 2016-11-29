ec46e7e00513f8ffb40a665a9583fc49e337afe3
Tliz <tlizzen@gmail.com>
2016-11-29 17:24:59 +0300

    Added comic book and artist models

---
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        // Attributes defined using properties
+        public int ID { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        //public string[] Artists { get; set; }
+        // change artist property to be array of artists
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
